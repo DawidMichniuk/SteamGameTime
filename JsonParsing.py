7e6ac4a854c9b5c91fa8eab706e65edd6d25292b
fategas <normalplek@gmail.com>
2019-07-11 23:08:03 +0200

    debugged the brackets

--- JsonParsing.py
+++ JsonParsing.py
@@ -48,4 +48,4 @@ for i in range(len(games)):
         total_time_played += games[i].get('playtime_forever')
 
 print("You've played a total of: "+ str(count) + " games, meaning you played only through about: " + str(round((count / y) * 100)) + " % of your games.")
-print("You have played a total of " + str(round(total_time_played, 3) + " hours across all your games."))
+print("You have played a total of " + str(round(total_time_played, 3)) + " hours across all your games.")
