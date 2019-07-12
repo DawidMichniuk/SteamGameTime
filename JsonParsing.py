c5170d06b9229b304183e1a150cd218e49a9911e
Fategas <38409508+Fategas@users.noreply.github.com>
2019-07-12 16:33:05 +0200

    Updated the file
    Fixed some errors that crashed the app, allowed to look up multiple steam ids, added file i/o and removed the console output.

--- JsonParsing.py
+++ JsonParsing.py
@@ -1,64 +1,90 @@
-# TODO: 1) sort the list so that you go from longest played games to the games
-# you have spend less time on.
-# 2) Somehow get access to prices someone has paid for these games.
+# TODO: 1) Somehow get access to prices someone has paid for these games.
 # https://store.steampowered.com/app/{appID} works well.
 # <div class="game_purchase_price price" data-price-final="719">£7.19</div>
+# 2) Put everything into different classes, create a main function and some sort of menu
+# so that you can check out multiple accounts instead of restarting the program every time
+# 3) save results to file named after steamID
 
 import json # appid and playtime comes from json file from steam
 import requests # used for web scrapping the data from steam's api website
 
 
-# Important!
-# Input your own key and steamID.
-print("input your API key: ")
-key = input()
-print("input your steamID: ")
-steamID = input()
+# except json.decoder.JSONDecodeError: # if key or id is wrong
+#    print("This key or steamID does not exist.")
+#    exit() # for now you just have to restart the program
+print("SteamGameTime counts how many hours have you spent on games in total across your steam library")
+print("In future versions it will also count the money per hour value of each game.")
+try:
+    # Setting up (opening) a file to read the API key from.
+    keyFile = open("key.txt", "r")
+    keyFromFile = keyFile.read()
+    if keyFromFile == "":
+        print("input your API key: ")
+        key = input()
+    else:
+        key = keyFromFile
+# If there is no key.txt file then just ask for the API key.
+except FileNotFoundError:
+    print("For future use, you can simply create a file called 'key.txt' with your key inside it.")
+    print("input your API key:")
+    key = input()
 
-# What is this magic link?
-# It connects to steam api to get all owned games by the user with the specified
-# steamID, along with their titles. Since we use steam's API, we needs a steam
-# developer API key to connect to said services. I can't share mine in fear of
-# some misusage, since I would be the one to blame.
-# The other arguments specify: the format of the data (json for easy data read),
-# To include app info(logo, icon and name) and the last one is for including
-# free to play games.
 
-websiteURL =  "http://api.steampowered.com/IPlayerService/GetOwnedGames/v0001/?key="+key+"&steamid="+steamID+"&format=json&include_appinfo=1&include_played_free_games=1"
+continueTheLoop = ""
+while continueTheLoop == "":
+    print("input your steamID: ")
+    steamID = input()
+    fileWithData = open((str(steamID) + ".txt"), "w+" )
 
-# This actually downloads the website so that python can process it.
-response = requests.get(websiteURL)
+    websiteURL =  "http://api.steampowered.com/IPlayerService/GetOwnedGames/v0001/?key="+key+"&steamid="+steamID+"&format=json&include_appinfo=1&include_played_free_games=1"
+    # This actually downloads the website so that python can process it.
+    response = requests.get(websiteURL)
 
-# This simply parses/loads the json file into text so that python can see it as
-# dictionaries and lists.
-testing = json.loads(response.text)
+    # This simply parses/loads the json file into text so that python can see it as
+    # dictionaries and lists.
+    mainJsonFile = json.loads(response.text)
 
-# First we want to get what's inside the "response" part of the dictionary
-x = testing.get("response")
+    # Get the total amount of games.
+    games_owned = (mainJsonFile.get("response")).get("game_count")
 
-# Get the amount of games with all the dlcs.
-y = x.get("game_count")
+    # 'games' is a list, while x is a dictionary
+    gamesList = (mainJsonFile.get("response")).get('games')
 
-# 'games' is a list, while x is a dictionary
-games = x.get('games')
+    # convert the number of games into string.
+    print("You have "+ str(games_owned)+ " games/applications.")
+    fileWithData.write("You have "+ str(games_owned)+ " games/applications.\n")
+    games_played = 0
+    total_time_played = 0
+    try:
+        # sort the list by playtime, from highest to lowest.
+        newlist = sorted(gamesList, key=lambda k: k["playtime_forever"], reverse=True)
 
-# convert the number of games into string.
-print("You have "+ str(y)+ " games/applications.")
+        for i in range(len(newlist)):
 
-games_played = 0
-total_time_played = 0
+            # if you actually played the game at all
+            if newlist[i].get('playtime_forever') > 0:
+                games_played += 1
+                fileWithData.write("You've spent " + str(round(float(newlist[i].get("playtime_forever") / 60), 3)) + "h playing "+ newlist[i].get("name") +".\n")
+                total_time_played += newlist[i].get('playtime_forever')
+            else:
+                list = []
+                list.append(newlist[i].get("name"))
 
-# sort the list by playtime, from highest to lowest.
-newlist = sorted(games, key=lambda k: k["playtime_forever"], reverse=True)
 
-# As long as there is something to get from the list of dictionaries.
-for i in range(len(newlist)):
-    # Only data in every iteration is appid and playtime forever
-    # if you actually played the game at all
-    if newlist[i].get('playtime_forever') > 0:
-        games_played += 1
-        print("You've spent " + str(round(float(newlist[i].get("playtime_forever") / 60), 3)) + "h playing "+ newlist[i].get("name") +".")
-        total_time_played += newlist[i].get('playtime_forever')
+        print("Done! Check out the results in " + str(steamID) + ".txt")
+        fileWithData.write("Games you own that you haven't played at all: ")
+        for i in range(len(list)):
+            fileWithData.write(list[i])
+        fileWithData.write("You've played a total of "+ str(games_played) + " games, meaning you played only through about " + str(round((games_played / games_owned) * 100)) + "% of your games.\n")
+        fileWithData.write("You have played a total of " + str(round((total_time_played / 60), 3) ) + " hours across all your games.\n")
 
-print("You've played a total of "+ str(games_played) + " games, meaning you played only through about " + str(round((games_played / y) * 100)) + "% of your games.")
-print("You have played a total of " + str(round((total_time_played / 60), 3) ) + " hours across all your games.")
+        print("If you want to check out another profile, simply press enter")
+        print("If not, write anything else, this will close the program")
+        continueTheLoop = input()
+
+    except TypeError:
+        print("Profile is either private or has no games")
+def closeFiles():
+    fileWithData.close()
+    keyFile.close()
+closeFiles()
